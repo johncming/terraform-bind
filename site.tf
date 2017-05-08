f7a138b10e044d747027b1ef25c65a4b9a04d8a1
johncming <johncming@yahoo.com>
2017-05-08 09:56:28 +0800

    add tf.

---
+++ site.tf
@@ -0,0 +1,45 @@
+variable "ip" {}
+
+variable "port" {
+  default = 2376
+}
+
+variable "bind_port" {
+  default = 53
+}
+
+variable "web_port" {
+  default = 10000
+}
+
+provider "docker" {
+  host = "tcp://${var.ip}:${var.port}/"
+}
+
+resource "docker_image" "bind" {
+  name = "sameersbn/bind:9.9.5-20170129"
+}
+
+resource "docker_container" "bind" {
+  image = "${docker_image.bind.latest}"
+  name  = "bind"
+
+  ports {
+    internal = 53
+    external = "${var.bind_port}"
+  }
+
+  ports {
+    internal = 10000
+    external = "${var.web_port}"
+  }
+
+  volumes {
+    volume_name = "${docker_volume.bind.name}"
+    container_path = "/data"
+  }
+}
+
+resource "docker_volume" "bind" {
+  name = "bind"
+}
